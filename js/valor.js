6de3a2ad97025f854baadca2d0e88244a29252ef
N3rdy <admin@n3rdydzn.xyz>
2022-06-09 08:28:49 -0300

    Deletando (a burrice)

--- js/valor.js
+++
@@ -1,9 +0,0 @@
-
-rsvalor = Math.floor(Math.random() * 150);
-centvalor = Math.floor(Math.random() * 99);
-
-function setar(){
-
-    txt = document.getElementById('valor');
-    txt.innerText = "Valor Total: R$" + rsvalor + "," + centvalor;
-}
